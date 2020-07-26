bcbc39a5e7b974769c4b950bab52e925ed2e73fd
tiriplicamihai <tiriplica.mihai@gmail.com>
2020-07-26 19:27:22 +0300

    Eval let statements.

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/tiriplicamihai/monkey/evaluator"
 	"github.com/tiriplicamihai/monkey/lexer"
+	"github.com/tiriplicamihai/monkey/object"
 	"github.com/tiriplicamihai/monkey/parser"
 )
 
@@ -26,6 +27,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -40,7 +42,7 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
